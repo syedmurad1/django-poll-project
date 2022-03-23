79a8905ad3ab12e31c376ef70b037a302b7641ca
Syed Murad <murad6000@yahoo.com>
2022-03-23 09:13:36 +0000

    commit

--- mypoll/polls/models.py
+++ mypoll/polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=250)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
@@ -11,7 +11,7 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=250)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
